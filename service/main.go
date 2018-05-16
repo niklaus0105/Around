ce342ec6ce7244a192065e85eb05bdc755f08c88
Nik <niklaus0105@gmail.com>
2018-05-16 17:29:24 -0500

    update ip_url

--- service/main.go
+++ service/main.go
@@ -23,7 +23,7 @@ const (
 //PROJECT_ID = "around-xxx"
 //BT_INSTANCE = "around-post"
 // Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.225.209.49:9200"
+	ES_URL = "http://23.251.144.34:9200"
 // Needs to update this bucket based on your gcs bucket name.
 	BUCKET_NAME = "post-images-2039211"
 
