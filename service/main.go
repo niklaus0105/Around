da4eb09fd0a24dd58b1ffa328ecbc98cafcd3057
Nik <niklaus0105@gmail.com>
2018-05-19 17:31:01 -0500

    add saveToBigTable

--- service/main.go
+++ service/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"cloud.google.com/go/bigtable"
 )
 
 const (
@@ -23,8 +24,8 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "sacred-lane-203921"
+	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.202.175.9:9200"
 	// Needs to update this bucket based on your gcs bucket name.
@@ -159,7 +160,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	saveToES(p, id)
 
 	// Save to BigTable.
-	//saveToBigTable(p, id)
+	saveToBigTable(p, id)
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -225,10 +226,6 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 }
 
 
-
-
-
-
 func saveToGCS(ctx context.Context, r io.Reader, bucket_name, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
 	// create a client
 	client, err := storage.NewClient(ctx)
@@ -289,6 +286,34 @@ func saveToES(p *Post, id string) {
 	}
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+}
+
+func saveToBigTable(p *Post, id string) {
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+	    panic(err)
+        return
+    }
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	// save data to row(mutation)
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+	    panic(err)
+	    return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+
 }
 
 func containsFilteredWords(s *string) bool {
